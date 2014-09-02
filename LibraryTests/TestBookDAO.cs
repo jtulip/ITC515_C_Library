b952cc98578c92941bd0d0aa0d5d803f7d14325e
jtulip <jtulip@csu.edu.au>
2014-09-02 12:06:42 +1000

    added test for TestBookDAO.cs

---
+++ LibraryTests/TestBookDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public class TestBookDAO
+    {
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            bookDict = new Dic
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
